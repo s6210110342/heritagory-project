d126bda99f137a8b1969c9d5df448c9d5f2c1645
6210110362 <6210110362@psu.ac.th>
2022-12-11 13:47:46 +0700

    edit

---
+++ src/components/islam/Calc.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Calc from '../islam/Calc';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Calc />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
