b56212b22396302563223db346540b841e01467c
6210110362 <6210110362@psu.ac.th>
2023-02-04 14:04:05 +0700

    edit

--- src/components/home/Banweb.jsx
+++ src/components/home/Banweb.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-      H <br />E <br />R <br />I <br />T <br />A <br />G <br />O <br />R <br />Y 
+      H <br />E <br />R <br />I <br />T <br />A <br />G <br />L <br/>A <br />W<br />S 
       
       <a href="https://lin.ee/njDD62n"><i class="uil uil-line text-[40px]"></i></a>
       
